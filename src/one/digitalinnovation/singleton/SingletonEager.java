98d4f208f6a50bd60fedd99c267cc5efa874d2ec
carlos738 <carlosasl135@hotmail.com>
2023-02-16 19:14:58 -0300

    primeiro commit

---
+++ src/one/digitalinnovation/singleton/SingletonEager.java
@@ -0,0 +1,12 @@
+package one.digitalinnovation.singleton;
+
+public class SingletonEager {
+    private static SingletonEager instance = new SingletonEager();
+    private SingletonEager(){
+        super();
+    }
+
+    public static SingletonEager getInstance() {
+        return instance;
+    }
+}
